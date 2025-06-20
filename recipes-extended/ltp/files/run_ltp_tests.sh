63e128a8b97faa6b690a32fdf4600cf3bbee7ad8
Chris Hofer <christian.hofer@codasip.com>
2025-06-20 16:27:22 +0200

    ltp: Improve script to run tests
    We add a check to ensure the module information is accessible for ltp in
    the standard location /lib/modules/$(uname -r). Additionally, some tests
    require to create files on non-tempfs mount points. So we move the
    temporary test directory to /home/root/ltp.
    
    Signed-off-by: Chris Hofer <christian.hofer@codasip.com>

--- recipes-extended/ltp/files/run_ltp_tests.sh
+++ recipes-extended/ltp/files/run_ltp_tests.sh
@@ -10,16 +10,35 @@ tstamp=$(date +"%Y-%m-%dT%H%M%S")
 # skip lists
 skipl="musl,qemu,riscv"
 
+# by default, test files are created in /tmp. Some tests require a non-tempfs
+# filesystem for their test files though. Move it to block dev mounted at "/".
+test_files_dir="/ltptmp"
+
 run_ltp_suite_tests() {
+
+    if [[ ! -d "/lib/modules/$(uname -r)" ]]; then
+        >&2 echo "ERROR: Module information not present in /lib/modules/$(uname -r)"
+        exit 1
+    fi
+
     : "${suite:?}"
+    test_tmp="${test_files_dir}_${suite}"
+    [[ -d "${test_tmp}" ]] && rm -rf "${test_tmp}"
+
+    # Some tests run as "nobody"
+    mkdir -m777 "${test_tmp}"
 
     LTP_TIMEOUT_MUL="${LTP_TIMEOUT_MUL}" \
     "${ltp}"/runltp \
         -p \
         -f "${suite}" \
+        -d "${test_tmp}" \
         -l "${logs}/ltp-${suite}_${tstamp}.log" \
         -C "${logs}/ltp-${suite}_${tstamp}.failed" \
         -S <( eval "cat ${ltp}/runtest/${suite}_codasip_{$skipl}_skip" )
+
+    # cleanup
+    rm -rf "${test_tmp}"
 }
 
 suite="syscalls" run_ltp_suite_tests
